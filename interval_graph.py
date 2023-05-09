6e712fe1fc7c19bae4b025336b438468ef3fdafb
Itasuka <schivre.nicolas@gmail.com>
2023-05-09 10:25:55 +0200

    Graph are now drawable on terminal and on a window with matplotlib

--- interval_graph.py
+++ interval_graph.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 def intervalGraphGen(order):
     remaining = [i for i in range(2 * order)]
     graph = []
@@ -9,8 +10,40 @@ def intervalGraphGen(order):
         graph.append((left,right))
     return graph
 
+
+def drawGraphTerminal(graph):
+    for interval in graph:
+        left = interval[0]
+        right = interval[1]
+        res = ""
+        for i in range(2*len(graph)):
+            if i == left:
+                res += "|"
+            elif i == right:
+                res += ">"
+            elif left < i < right:
+                res += "-"
+            else:
+                res += " "
+        print(res)
+
+def drawGraphWindow(graph):
+    y_pos = range(len(graph))
+    longueurs = [end - start for start, end in graph]
+    start_points = [start for start, end in graph]
+    end_points = [end for start, end in graph]
+    plt.xticks(start_points+end_points, "")
+    plt.grid(axis='x', linestyle='--', linewidth = 0.5)
+    plt.yticks([])
+    plt.ylabel('')
+    plt.barh(y_pos, longueurs, left=start_points)
+    plt.show()
+
+
 if __name__ == '__main__':
     order = int(input("Please enter the graph order : "))
     graph = intervalGraphGen(order)
     for interval in graph:
         print("left : ", interval[0], "; right : ",interval[1])
+    drawGraphTerminal(graph)
+    drawGraphWindow(graph)
