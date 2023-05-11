9f7e15be9f84efa1186aff87fea1cb3bde34f6f5
Itasuka <schivre.nicolas@gmail.com>
2023-05-11 17:45:07 +0200

    QTRD check function implemented QTRD brute force solution implemented Graph are now drawable with differents colors based on the solution

--- interval_graph.py
+++ interval_graph.py
@@ -1,24 +1,30 @@
+import itertools
 import random
 import timeit
+from collections import OrderedDict
 
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
+
 
 def intervalGraphGen(order):
     remaining = [i for i in range(2 * order)]
     graph = []
-    while remaining :
-        i = random.randint(0,len(remaining)-2)
+    while remaining:
+        i = random.randint(0, len(remaining) - 2)
         left = remaining.pop(i)
-        right = remaining.pop(random.randint(i,len(remaining)-1))
-        graph.append((left,right))
+        right = remaining.pop(random.randint(i, len(remaining) - 1))
+        graph.append((left, right))
     return graph
+
 
 def drawGraphTerminal(graph):
     for interval in graph:
         left = interval[0]
         right = interval[1]
         res = ""
-        for i in range(2*len(graph)):
+        for i in range(2 * len(graph)):
             if i == left:
                 res += "|"
             elif i == right:
@@ -29,21 +35,36 @@ def drawGraphTerminal(graph):
                 res += " "
         print(res)
 
-def drawGraphWindow(graph):
+
+def drawGraphWindow(graph, colors=None, labels=None):
+    if labels is None:
+        labels = []
+    if colors is None:
+        colors = ['steelblue']
     y_pos = range(len(graph))
     longueurs = [end - start for start, end in graph]
     start_points = [start for start, end in graph]
     end_points = [end for start, end in graph]
-    plt.xticks(start_points+end_points, start_points+end_points)
-    plt.grid(axis='x', linestyle='--', linewidth = 0.5)
+    plt.xticks(start_points + end_points, start_points + end_points)
+    plt.grid(axis='x', linestyle='--', linewidth=0.5)
     plt.yticks([])
     plt.ylabel('')
-    plt.barh(y_pos, longueurs, left=start_points)
+    legend_elements = []
+    unique_colors = OrderedDict()
+    for color, label in zip(colors, labels):
+        if color not in unique_colors:
+            unique_colors[color] = label
+            legend_elements.append(Patch(facecolor=color))
+    for i, color in enumerate(unique_colors.keys()):
+        legend_elements[i].set_label(unique_colors[color])
+    plt.barh(y_pos, longueurs, left=start_points, color=colors)
+    plt.legend(handles=legend_elements)
     plt.show()
 
 
 def order(graph):
     return len(graph)
+
 
 def neighbor(graph, v):
     l_v = v[0]
@@ -56,19 +77,22 @@ def neighbor(graph, v):
             neighbor.append(interval)
     return neighbor
 
+
 def closedNeighbor(graph, v):
-    return neighbor(graph,v) + [v]
+    return neighbor(graph, v) + [v]
+
 
 def neighborhood(graph):
     neighborhood = []
     for interval in graph:
-        neighborhood.append(neighbor(graph,interval))
+        neighborhood.append(neighbor(graph, interval))
     return neighborhood
+
 
 def closedNeighborhood(graph):
     closedNeighborhood = []
     for interval in graph:
-        closedNeighborhood.append(closedNeighbor(graph,interval))
+        closedNeighborhood.append(closedNeighbor(graph, interval))
     return closedNeighborhood
 
 
@@ -77,14 +101,15 @@ if __name__ == '__main__':
     start = timeit.default_timer()
     graph = intervalGraphGen(order)
     stop = timeit.default_timer()
-    print("time : ",stop - start)
+    print("time : ", stop - start)
     for interval in graph:
-        print("left : ", interval[0], "; right : ",interval[1])
-        print("neighbor = ", neighbor(graph,interval))
-        print("closedneighbor = ", closedNeighbor(graph,interval))
+        print("left : ", interval[0], "; right : ", interval[1])
+        print("neighbor = ", neighbor(graph, interval))
+        print("closedneighbor = ", closedNeighbor(graph, interval))
     drawGraphTerminal(graph)
-    sorted_graph = sorted(graph,key=lambda x:len(closedNeighbor(graph,x)))
-    print("Result : ",sorted_graph)
+    sorted_graph = sorted(graph, key=lambda x: len(closedNeighbor(graph, x)))
+    print("Result : ", sorted_graph)
     for interval in sorted_graph:
-        print("Interval = ", interval, "\nDegree : ", len(closedNeighbor(sorted_graph,interval)))
+        print("Interval = ", interval, "\nDegree : ", len(closedNeighbor(sorted_graph, interval)))
     drawGraphWindow(sorted_graph)
+    allGraphFromOrder(order)
--- qtrd.py
+++ qtrd.py
@@ -1,4 +1,5 @@
 import interval_graph
+import itertools
 
 
 def dominated(graph, v, V1, V2):
@@ -55,14 +56,66 @@ def qtrd_v1(graph):
     return res
 
 
+def qtrdChecker(graph, solution):
+    v0, v1, v2 = solution[0], solution[1], solution[2]
+    qtrdNumber = len(v1) + 2 * len(v2)
+    for interval in graph:
+        if interval not in v0 + v1 + v2:
+            return -1
+        if not dominated(graph, interval, v1, v2):
+            return -1
+    for v in v2:
+        if isolated(graph, v, v1, v2):
+            return -1
+    return qtrdNumber
+
+
+def qtrdBruteForce(graph):
+    minSolution = float("inf")
+    possibilities = itertools.product([0, 1, 2], repeat=len(graph))
+    for possibility in possibilities:
+        solution = {0: [], 1: [], 2: []}
+        type = list(possibility)
+        for i in range(len(graph)):
+            solution[type[i]].append(graph[i])
+        value = qtrdChecker(graph,solution)
+        if value != -1:
+            minSolution = min(value,minSolution)
+    return minSolution
+
+
+def drawWithSolution(solution):
+    v0,v1,v2 = solution[0],solution[1],solution[2]
+    colors = []
+    labels = []
+    for v in v0:
+        colors.append('steelblue')
+        labels.append('V0')
+    for v in v1:
+        colors.append('forestgreen')
+        labels.append('V1')
+    for v in v2:
+        colors.append('firebrick')
+        labels.append('V2')
+    interval_graph.drawGraphWindow(v0 + v1 + v2, colors, labels)
+
+
 if __name__ == '__main__':
     order = int(input("Please enter the graph order : "))
     graph = interval_graph.intervalGraphGen(order)
     sorted_graph = sorted(graph, key=lambda x: x[1])
     sorted_graph = sorted(sorted_graph, key=lambda x: len(interval_graph.closedNeighbor(graph, x)))
-    interval_graph.drawGraphWindow(sorted_graph)
+    #interval_graph.drawGraphWindow(sorted_graph)
 
     res = qtrd_v1(graph)
     print("V0 : ", res[0])
     print("V1 : ", res[1])
     print("V2 : ", res[2])
+    print("Graph : ", graph)
+    drawWithSolution(res)
+
+    check = qtrdChecker(graph, res)
+    print("Check Value : ", check)
+
+    minSolution = qtrdBruteForce(graph)
+    print("Minimum solution : ",minSolution)
